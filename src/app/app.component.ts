3e79cfb94cefb2098d35b785e30b931737fa38be
MustafaK01 <mestefe119@gmail.com>
2023-06-17 03:06:09 +0300

    add filter functionality, spinner, input control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NearByPlace } from './model/NearbyPlace.model';
 import { MapOptions } from './model/Options.model';
+import { SpinnerService } from './services/spinner.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +16,24 @@ export class AppComponent implements OnInit{
   longitude = 0;
   radius = 1500; 
   circleUpdated = false;
+
   nearByPlaces: NearByPlace[] = [];
+  healthCenters:NearByPlace[] = [];
+  restaurants:NearByPlace[] = [];
+  superMarkets:NearByPlace[] = [];
+  atms:NearByPlace[] = [];
+
+  showAll = true;
+
+  selectedShowAtms = false;
+  selectedShowSuperMarkets=false;
+  selectedShowHealthCenters=false;
+  selectedShowRestaurants=false;
+
   center: google.maps.LatLngLiteral = { lat: 49, lng: 12 };
   mapOptions: MapOptions;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,private spinnerService:SpinnerService, private toastr: ToastrService) {
     this.mapOptions = new MapOptions();
    }
 
@@ -40,7 +55,66 @@ export class AppComponent implements OnInit{
     this.longitude = event.latLng.lng();
   }
 
+  filterHealthCenters(place){
+    let healthCenterKeyWords = ["hospital","doctor"]
+    this.filterPlace(place,healthCenterKeyWords,this.healthCenters);
+  }
+
+  filterRestaurants(place){
+    let restaurantKeywords = ["restaurant","bar","cafe"];
+    this.filterPlace(place,restaurantKeywords,this.restaurants);  
+  }
+
+  filterSupermarkets(place){
+    let supermarketKeywords = ["supermarket"];
+    this.filterPlace(place,supermarketKeywords,this.superMarkets);  
+  }
+
+  filterAtms(place){
+    let atmKeywords = ["atm","finance","bank"];
+    this.filterPlace(place,atmKeywords,this.atms);  
+  }
+
+  filterPlaces(place){
+    this.filterHealthCenters(place);
+    this.filterRestaurants(place);
+    this.filterSupermarkets(place);
+    this.filterAtms(place);
+  }
+
+  showRestaurants(){
+    this.selectedShowRestaurants=true;
+    this.showAll=false;
+    this.selectedShowAtms=false;
+    this.selectedShowHealthCenters=false;
+    this.selectedShowSuperMarkets=false;
+  }
+
+  showAtms(){
+    this.atms.forEach(element => {
+      console.log(element);
+    });
+  }
+
+  showSupermarkets(){
+    this.superMarkets.forEach(element => {
+      console.log(element);
+    });
+  }
+
+  showHealthCenters(){
+    this.healthCenters.forEach(element => {
+      console.log(element);
+    });
+  }
+
   searchByLatLngAndRadius(){
+    this.nearByPlaces = [];
+    if(this.checkInputs()){ 
+      this.toastr.error('Please provide latitude and longitude', 'ERROR');
+      return;
+    }
+    this.spinnerService.requestStarted();
     this.http.get('http://localhost:8070/api/location-service/getNearbyPlaces?latitude='+this.latitude
     +'&longitude='+this.longitude+'&radius='+this.radius).subscribe((response:any) => {
       console.log(response.nearbyPlaces);
@@ -48,28 +122,33 @@ export class AppComponent implements OnInit{
         let place = new NearByPlace(element.latitude,element.longitude
           ,element.name,element.rating,element.types,element.vicinity);
           if(this.checkLocationDistance(place.latitude,place.longitude)) {
-            this.nearByPlaces.push(place);
+            this.nearByPlaces.push(place);          
+            this.filterPlaces(place);
           }
-      });
+          
+      },  this.spinnerService.requestEnded(),
+      console.log(this.healthCenters)
+
+      );
     }, (error) => {
       console.error(error);
     });
   }
 
   clearMap(){
+    this.spinnerService.requestStarted();;
     this.nearByPlaces = [];
     this.latitude = 0;
     this.longitude = 0;
+    setTimeout(() => {
+      this.spinnerService.requestEnded();
+    }, 1000)
   }
-
 
   //The control is also performed on the backend side, but just in case, 
   //i wanted to check here as well to ensure accurate results. 
   //Anyone who wants to test whether the backend is functioning or not, can remove the 
   //'if' block under the subscribe function and try.
-  checkLocationDistance(latitude,longitude){
-    return this.calculateDistance(this.latitude,this.longitude,latitude,longitude) < this.radius;
-  }
 
   calculateDistance(lat1: number, lon1: number, lat2: number, lon2: number): number {
     const earthRadius = 6371;
@@ -91,5 +170,23 @@ export class AppComponent implements OnInit{
     return distance;
   }
 
+  filterPlace(place,keywords,arr){
+    for (let i = 0; i < place.types.length; i++) {
+      for (let j = 0; j < keywords.length; j++) {
+        if(place.types[i]===keywords[j]){
+          arr.push(place);
+          i=place.types.length;
+        }         
+      }
+    }
+  }
+
+  checkInputs(){
+    return this.latitude == 0 && this.longitude == 0 || this.radius==0;
+  }
+
+  checkLocationDistance(latitude,longitude){
+    return this.calculateDistance(this.latitude,this.longitude,latitude,longitude) < this.radius;
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
     GoogleMapsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
